d77d5033b2bc13c6d6249766f7877b04362696f0
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-05-23 17:42:05 -0400

    added all test files from the note app

---
+++ note-app/playground/arrow-function.js
@@ -0,0 +1,22 @@
+// // // var square = x => x*x;
+// // // console.log(square(7));
+// //
+// //
+// //
+// // var user = {
+// //   name: 'Arjun',
+// //   sayHi: () => {
+// //     console.log(arguments);
+// //     console.log(`Hi. I'm ${this.name}`);
+// //   },
+// //   sayHiAlt () {
+// //     console.log(arguments);
+// //     console.log(`Hi. I'm ${this.name}`);
+// //   }
+// // };
+// //
+//
+// function sayHi(){
+//   console.log(`Hi. I'm Arjun`);
+// }
+// sayHi();
---
+++ note-app/playground/debugging.js
@@ -0,0 +1,9 @@
+var person = {
+  name: 'Arjun'
+};
+
+person.age = 18;
+debugger;
+person.name = 'Mike';
+
+console.log(person);
