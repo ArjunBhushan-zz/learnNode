c5d58a05f70b0ac2fdb69c82ab13dda5ee758efa
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-06-15 22:11:28 -0400

    Add GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -4,6 +4,7 @@ var bodyParser= require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/todo');
+var {ObjectId} = require('mongodb');
 
 var app = express();
 var port = 3000;
@@ -33,6 +34,27 @@ app.get('/todos', (req,res) => {
       res.status(400).send(err);
     });
 });
+
+//GET /todos/
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    res.status(404).send();
+  }else{
+    Todo.findById(id)
+      .then((todo) => {
+        if(!todo){
+          res.status(404).send();
+        }
+        res.status(200).send({todo});
+      })
+      .catch((err) => {
+        res.status(400).send();
+      });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
