276552673b448a135a55a4d52d76ced5e54e3fb3
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-06-16 10:48:02 -0400

    Add DELETE /todos/:id route

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 //sudo mongod --storageEngine=mmapv1 --dbpath mongo-local/
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:password1@ds261440.mlab.com:61440/node-notes-db');
-
+if (process.env.PORT){
+  mongoose.connect('mongodb://admin:password1@ds261440.mlab.com:61440/node-notes-db');
+}else{
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 module.exports = {
   mongoose
 };
