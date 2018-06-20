7387e2b5c7b2c684bbf56517d3fe257d5e1f393f
Arjun_Bhushan <arjun_bhushan@cooperators.ca>
2018-06-20 11:16:57 -0400

    Add geolocation support

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -14,14 +14,40 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(locationMessage){
+  console.log(locationMessage);
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>')
+  a.attr('href', locationMessage.url);
+  li.text(`${locationMessage.from}: `);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
-
   socket.emit('createMessage', {
     from: 'User',
     text: jQuery('[name="message"]').val()
   }, function(data){
     console.log(data);
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation){
+    return alert('Gelocation not support by your browser.');
+  }
+  navigator.geolocation.getCurrentPosition(function (position){
+    var geolocation = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }
+    socket.emit('createLocationMessage', geolocation, function(data){
+      console.log(data);
+    });
+  }, function(){
+    alert('Unable to fetch location.');
   });
 });
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -4,6 +4,7 @@ const express  = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage} = require('./utils/message');
+const {generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
@@ -28,6 +29,11 @@ io.on('connection', (socket) => {
     });
   });
 
+  socket.on('createLocationMessage', (coords, callback) => {
+    console.log('createLocationMessage', coords);
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    callback('Location successfully sent');
+  });
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
