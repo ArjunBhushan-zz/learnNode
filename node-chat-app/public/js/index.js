7eb4436ec022feceecb90e7fb95e18b79af597f4
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-06-20 18:39:20 -0400

    Add mustache.js for message templates

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -8,23 +8,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('newMessage: ', message);
+  console.log(message);
+  var template = jQuery('#message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(locationMessage){
   console.log(locationMessage);
   var formattedTime = moment(locationMessage.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>')
-  a.attr('href', locationMessage.url);
-  li.text(`${locationMessage.from} ${formattedTime}: `);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: locationMessage.from,
+    url: locationMessage.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e){
