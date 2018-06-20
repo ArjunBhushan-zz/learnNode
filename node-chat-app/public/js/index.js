eea4dcea32cfe0e580758f9908d6f307460644e4
Firstname Lastname <arjun.bhushan2@gmail.com>
2018-06-19 21:42:14 -0400

    Emit newMessage on createMessage

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'example@hotmail.com',
-    text: 'buddy, how is this working?'
-  });
 });
 
 socket.on('disconnect', function() {
